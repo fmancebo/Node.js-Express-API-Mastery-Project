83732a979a7f3be9fabd88f8c38b8690e1b75288
fmancebo <f.92xavier1@gmail.com>
2024-07-12 16:58:38 -0300

    processo de autenticaçao finalizado

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,37 @@
 import { Router } from "express";
-import customers from "./app/controllers/customersControllers";
+import auth from "./app/middlewares/auth";
+import sessions from "./app/controllers/SessionsController";
+import customers from "./app/controllers/CustomersController";
+import contacts from "./app/controllers/ContactsController";
+import users from "./app/controllers/UsersController";
 
 const routes = new Router();
 
+// Sessions
+routes.post("/sessions", sessions.create);
+
+// controla o acesso desse ponto
+routes.use(auth);
+
+// Customers
 routes.get("/customers", customers.index);
 routes.get("/customers/:id", customers.show);
-routes.post("/customers/", customers.create);
+routes.post("/customers", customers.create);
 routes.put("/customers/:id", customers.update);
 routes.delete("/customers/:id", customers.destroy);
+
+// Contacts
+routes.get("/customers/:customerId/contacts", contacts.index);
+routes.get("/customers/:customerId/contacts/:id", contacts.show);
+routes.post("/customers/:customerId/contacts", contacts.create);
+routes.put("/customers/:customerId/contacts/:id", contacts.update);
+routes.delete("/customers/:customerId/contacts/:id", contacts.destroy);
+
+// Users
+routes.get("/users", users.index);
+routes.get("/users/:id", users.show);
+routes.post("/users", users.create);
+routes.put("/users/:id", users.update);
+routes.delete("/users/:id", users.destroy);
 
 export default routes;
